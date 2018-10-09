1c9f4e24f03fdbcf2dd967c3da0a8c2d61a7c1e7
Mrs0sTamvan <Mr.s0sTamvan@gmail.com>
2018-10-09 20:21:18 +0700

    Upload di Github

---
+++ Tools.sh
@@ -0,0 +1,558 @@
+#system/bin/sh
+clear
+blue="\033[34;1m"
+green="\033[32;1m"  
+purple="\033[35;1m"
+cyan="\033[36;1m"
+red="\033[31;1m"
+white="\033[37;1m"                                           
+yellow="\033[33;1m"
+normal="\033[0m"
+cd cuk
+python kontol.py
+echo -e $red
+figlet -f shadow Tools-Fuck
+echo -e $red "========================================>"
+echo -e $green "Author = PangeraN And Mr.sOs"
+echo -e $cyan "Thanx To = We . Me .and You"
+echo -e $white "Gretz To= JackZors,Black Box, And Termux Association"
+echo -e $green "=======================================>"
+
+
+##############################
+# CTRL C
+#############################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo -e $cyan"[×]Ctrl-C terdeteksi cuk..."
+sleep 1
+echo -e $cyan"[×]Thanks ea Bangsad:)"
+exit
+}
+
+echo ""
+echo -e $cyan "=======================================>"
+echo -e "1 <.Spam Sms Jdid"
+echo -e "=============================================>"
+echo -e $red "[*]"
+echo -e $cyan "=======================================>"
+echo -e "2 <.Instal Metasploit"
+echo -e "=============================================>"
+echo -e $red "[*]"
+echo -e $cyan "=======================================>"
+echo -e "3 <.Ganti Tema Termux"
+echo -e "=============================================>"
+echo -e $red "[*]"
+echo -e $cyan "=======================================>"
+echo -e "4 <.install LITEDDOS buat ddos website"
+echo -e "=============================================>"
+echo -e $red "[*]"
+echo -e $cyan "=======================================>"
+echo -e "5 <.Full Bot facebook"
+echo -e "=============================================>"
+echo -e $red "[*]"
+echo -e $cyan "=======================================>"
+echo -e "6 <.Install Brutall"
+echo -e "=============================================>"
+echo -e $red "[*]"
+echo -e $cyan "=======================================>"
+echo -e "7 <.Spam sms LITEOTP"
+echo -e "=============================================>"
+echo -e $red "[*]"
+echo -e $cyan "=======================================>"
+echo -e "8 <.install hydra"
+echo -e "=============================================>"
+echo -e $red "[*]"
+echo -e $cyan "=======================================>"
+echo -e "9 <.Install santet-onlen"
+echo -e "=============================================>"
+echo -e $red "[*]"
+echo -e $cyan "=======================================>"
+echo -e "10 <..install Hash Cracker"
+echo -e "=============================================>"
+echo -e $red "[*]"
+echo -e $cyan "=======================================>"
+echo -e "11 <.Tools BAJINGANv6"
+echo -e "=============================================>"
+echo -e $red "[*]"
+echo -e $cyan "=======================================>"
+echo -e "12 <.Brute Force Fb"
+echo -e "=============================================>"
+echo -e $red "[*]"
+echo -e $cyan "=======================================>"
+echo -e "13 <.Install LITESPAM"
+echo -e "=============================================>"
+echo -e $red "[*]"
+echo -e $cyan "=======================================>"
+echo -e "14 <.install Bot Komena fb"
+echo -e "=============================================>"
+echo -e $red "[*]"
+echo -e $cyan "=======================================>"
+echo -e "15 <.Install weeman"
+echo -e "=============================================>"
+echo -e $red "[*]"
+echo -e $cyan "=======================================>"
+echo -e "16 <.Install striker"
+echo -e "=============================================>"
+echo -e $red "[*]"
+echo -e $cyan "=======================================>"
+echo -e "17 <.Install Tools Mr.cakil"
+echo -e "=============================================>"
+echo -e $red "[*]"
+echo -e $cyan "=======================================>"
+echo -e "18 <.Install Osif"
+echo -e "=============================================>"
+echo -e $red "[*]"
+echo -e $cyan "=======================================>"
+echo -e "19 <.Install Scanner-Inurlbr"
+echo -e "=============================================>"
+echo -e $red "[*]"
+echo -e $cyan "=======================================>"
+echo -e "20 <.Install crack Password hash "
+echo -e "=============================================>"
+echo -e $red "[*]"
+echo -e $cyan "=======================================>"
+echo -e "21 <.Install spammer grab"
+echo -e "=============================================>"
+echo -e $red "[*]"
+echo -e $cyan "=======================================>"
+echo -e "22 <.Auto bot Facebook"
+echo -e "=============================================>"
+echo -e $red "[*]"
+echo -e $cyan "=======================================>"
+echo -e "23 <.Fb auto-reaction"
+echo -e "=============================================>"
+echo -e $red "[*]"
+echo -e $cyan "=======================================>"
+echo -e "24 <.Install Hackinsta"
+echo -e "=============================================>"
+echo -e $red "[*]"
+echo -e $cyan "=======================================>"
+echo -e "25 <.Localizar ip"
+echo -e "=============================================>"
+echo -e $red "[*]"
+echo -e $cyan "=======================================>"
+echo -e "26 <.Red_Hawk__Tool"
+echo -e "=============================================>"
+echo -e $red "[*]"
+echo -e $cyan "=======================================>"
+echo -e "27 <.Hammer #Buat ddos"
+echo -e "=============================================>"
+echo -e $red "[*]"
+echo -e $cyan "=======================================>"
+echo -e "28 <.Xexres #buat ddos"
+echo -e "=============================================>"
+echo -e $red "[*]"
+echo -e $cyan "=======================================>"
+echo -e "29 <.Torshammer #buat ddos"
+echo -e "=============================================>"
+echo -e $red "[*]"
+echo -e $cyan "=======================================>"
+echo -e "30 <.Webdav #deface"
+echo -e "=============================================>"
+echo -e $red "[*]"
+echo -e $cyan "=======================================>"
+echo -e "31 <.Tembak Paket XL"
+echo -e "=============================================>"
+echo -e $red "[*]"
+echo -e $cyan "=======================================>"
+echo -e "32 <.Admin panel Finder"
+echo -e "=============================================>"
+echo -e $red "[*]"
+echo -e $cyan "=======================================>"
+echo -e "34 <.Install tools Mr.rv1.1"
+echo -e "=============================================>"
+echo -e $red "[*]"
+echo -e $cyan "=======================================>"
+echo -e "35 <.install tools fsociety"
+echo -e "=============================================>"
+echo -e $red "[*]"
+echo -e $cyan "=======================================>"
+echo -e "36 <.Tools By Mr.sOs"
+echo -e "=============================================>"
+echo -e $red "[*]"
+echo -e $cyan "=======================================>"
+echo -e "37 <.Dtect-Tools"
+echo -e "=============================================>"
+echo -e $red "[*]"
+echo -e $cyan "=======================================>"
+echo -e "38 <.Maen Moon-Buggy cok"
+echo -e "=============================================>"
+echo -e $red"[*]"
+echo -e $cyan "=======================================>"
+echo -e "39 <.Musikan Di Termux biar ga bego"
+echo -e "=============================================>"
+echo -e $red"[*]"
+echo -e $cyan "=======================================>"
+echo -e "40 <.brosing di termos"
+echo -e "=============================================>"
+echo -e $red"[*]"
+echo -e $cyan "=======================================>"
+echo -e "41 <.yutub termux"
+echo -e "=============================================>"
+echo -e $red"[*]"
+echo -e $cyan "=======================================>"
+echo -e "42 <.Hakku framework"
+echo -e "=============================================>"
+echo ""
+echo -e "Satu Tools 5K Oke, Becanda Coeg"
+echo -e $green
+read -p "ToolFuck@Root=>#" bro;
+echo -e $cyan
+
+case $bro in
+1) figlet -f shadow installer
+pkg install php
+apt install wget
+wget https://pastebin.com/raw/9BYy1JVc-ojdid.php
+php jdid.php
+
+;;
+
+2) figlet -f shadow installer
+curl -LO https://raw.githubusercontent.com/Hax4us/Metasploit_termux/master/metasploit.sh 
+chmod metasploit.sh
+sh metasploit.sh
+cd metasploit-framework/
+bundle install
+bundle install Nokogiri
+./msfconsole
+
+;;
+
+
+3) figlet -f shadow installer
+git clone https://github.com/Cabbagec/termux-ohmyzsh
+cd termux-ohmyzsh
+sh install.sh
+
+;;
+
+
+4) figlet -f shadow installer
+git clone https://github.com/4L13199/LITEDDOS 
+cd LITEDDOS
+python2 liteDDOS.py
+
+;;
+
+
+5) figlet -f shadow installer
+git clone https://github.com/Senitopeng/BotFbBangDjon.git 
+cd BotFbBangDjon
+python2 bangdjon.py
+
+;;
+
+
+6) figlet -f shadow installer
+git clone https://github.com/Screetsec/Brutal
+cd Brutal
+sh Brutal.sh
+
+;;
+
+
+7) figlet -f shadow installer
+git clone https://github.com/Cvar1984/LiteOTP
+cd LiteOTP
+chmod 777 lite.php
+php lite.php
+
+;;
+
+
+8) figlet -f shadow installer
+git clone https://github.com/Gameye98/Black-hydra
+cd b-hydra
+mv blackhydra.py main.py
+chmod +x main.py
+python2 main.py
+
+;;
+
+
+9) figlet -f shadow installer
+git clone https://github.com/Gameye98/santet-online
+cd santet-online
+python2 santet.py
+
+;;
+
+
+10) figlet -f shadow installer
+git clone https://github.com/UltimateHackers/Hash-Buster
+cd Hash-Buster
+python2 hash.py
+
+;;
+
+
+11) figlet -f shadow installer
+git clone https://github.com/DarknessCyberTeam/BAJINGANv6
+cd BAJINGANv6
+sh BAJINGAN.sh
+
+;;
+
+
+12) figlet -f shadow installer
+git clone https://github.com/Senitopeng/fbbrute
+cd fbbrute
+python2 MBF.py
+
+;;
+
+
+13) figlet -f shadow installer
+git clone https://github.com/4L13199/LITESPAM
+cdLITESPAM
+sh LITESPAM.sh
+
+;;
+
+
+14) figlet -f shadow installer
+git clone https://github.com/Senitopeng/Botkomena.git
+cd Botkomena
+python2 botkomena.py
+
+;;
+
+
+15) figlet -f shadow installer
+git clone https://github.com/evait-security/weeman.git
+cd weeman
+python2 weeman.py
+
+;;
+
+
+16) figlet -f shadow installer
+git clone https://github.com/UltimateHackers/Striker
+cd Striker
+pip2 install -r requirements.txt
+python2 striker.py
+
+;;
+
+
+17) figlet -f shadow installer
+git clone https://github.com/mrcakil/Mrcakil.git
+cd Mrcakil
+chmod +x tools
+./tools
+
+;;
+
+
+18) figlet -f shadow installer
+git clone https://github.com/CiKu370/OSIF.git
+cd OSIF
+pip2 install -r requirements.txt
+python2 osif.py -i 
+
+;;
+
+
+19) figlet -f shadow installer
+git clone https://github.com/googleinurl/SCANNER-INURLBR.git
+cd SCANNER-INURLBR
+php inurlbr.php
+
+;;
+
+
+20) figlet -f shadow installer
+git clonie https://github.com/UltimateHackers/Hash-Buster
+cd Hash-Buster
+python2 hash.py
+
+;;
+
+
+21) figlet -f shadow installer
+git clone https://github.com/p4kl0nc4t/Spammer-Grab
+cd Spammer-Grab
+chmod+xspammer.py
+python2 spammer.py
+
+;;
+
+22) figlet -f shadow installer
+git clone https://github.com/Senitopeng/BotFbBangDjon.git 
+cd BotFbBangDjon
+python2 bangdjon.py
+
+;;
+
+23) figlet -f shadow installer
+git clone https://github.com/tomiashari/fb-autoreaction.git
+cd fb-autoreaction
+python2 fb-autoreaction.py
+
+;;
+
+24) figlet -f shadow installer
+git clone https://github.com/avramit/instahack.git
+cd instahack
+pip install requests
+cd instahack
+python2 hackinsta.py
+
+;;
+
+
+25) figlet -f shadow installer
+git clone https://github.com/maldevel/IPGeoLocation.git
+cd IPGeoLocation
+chmod +x ipgeoLocation.py
+pip install -r requirements.txt
+python ipgeolocation.py -m
+
+;;
+
+26) figlet -f shadow installer
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+cd RED_HAWK
+chmod +x rhawk.php
+php rhawk.php
+
+;;
+
+
+27) figlet -f shadow installer
+git clone https://github.com/cyweb/hammer
+cd hammer 
+python hammer.py
+
+;;
+
+28) figlet -f shadow installer
+git clone https://github.com/zanyarjamal/xerxes
+cd xerxes
+clang xerxes.c -o xerxes
+
+;;
+
+29) figlet -f shadow installer
+git clone https://github.com/dotfighter/torshammer.git 
+cd torshammer
+python2 torshammer.py
+
+;;
+
+30) figlet -f shadow installer
+apt update
+upgrade
+apt install python2
+pip2 install urllib3 chardet certifi idna requests 
+apt install openssl curl 
+pkg install libcurl 
+ln -s /sdcard 
+cd sdcard 
+mkdir webdav 
+cd webdav
+
+;;
+
+31) figlet -f shadow installer
+git clone https://github.com/albertoanggi/xl-py
+pip install -r requirements.txt
+chmod +x app.py
+python2 app.py
+
+;;
+
+32) figlet -f shadow installer
+git clone https://github.com/AdheBolo/AdminFinder 
+cd AdminFinder
+chmod 777 AdminFinder.py
+python2 AdminFinder.py
+
+;;
+
+33) figlet -f shadow installer
+git clone https://github.com/Techzindia/admin_penal/
+cd adminpanel
+mv admin_panel_finder.py main.py
+chmod +x main.py
+python2 main.py
+
+;;
+
+34) figlet -f shadow installer
+https://github.com/Mr-R225/Mr.rv1.0
+cd Mr.rv1.0
+chmod +x *
+sh Mr.Rv1.0.sh
+
+;;
+
+35) figlet -f shadow installer
+git clone https://github.com/Manisso/fsociety
+cd fsociety
+python2 fsociety.py 
+
+;;
+
+36) figlet -f shadow installer
+http://github.com/Mrs0sTamvan/MrsOs
+cd MrsOs
+chmod 777 MrsOs
+./MrsOs
+
+;;
+
+
+37) figlet -f shadow installer
+git clone https://github.com/shawarkhanethicalhacker/D-TECT
+cd D-TECH
+chmod +x d-tect.py
+python2 d-tect.py
+
+;;
+
+38) figlet -f shadow installer
+pkg install moon-buggy
+moon-buggy
+
+;;
+
+39) figlet -f shadow installer
+pkg install mpv
+mpv/sdcard/lagu.mp3
+
+;;
+
+40) figlet -f shadow installer
+pkg install w3m
+w3m www.google.com
+
+;;
+
+41) figlet -f shadow installer
+pip install mps_youtube
+pip install youtube_dl
+apt install mpv
+mpsyt
+/judul lagu
+
+;;
+
+42) figlet -f shadow installer
+mkdir vasu
+git clone https://github.com/4shadoww/hakkuframework
+cd hakkuframework
+chmod +x hakku
+python hakku
+show modules
+use whois
+show options
+
+;;
+
+*) echo -e $red "PILIH YANG BENER ANJING"
+esac
+
